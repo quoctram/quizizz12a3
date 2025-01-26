08afba605e408a4c9b69bed62ddaff5e2594af56
quoctram <quoctram230207@gmail.com>
2025-01-26 11:33:54 +0700

    capnhat00001

---
+++ main.js
@@ -0,0 +1,22 @@
+// Mã hóa kí tự
+function maHoa(kiTu){
+    const a="ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwxyz0123456789";
+    const b="ZXCVBNMASDFGHJKLQWERTYUIOPzxcvbnmasdfghjklqwertyuiop9876543210";
+    let result="";
+
+    for(let i=0; i<kiTu.length;i++){
+        let char = kiTu[i];
+        if(a.includes(char)){
+            let index = a.indexOf(char);
+            result+=b[index];
+        }
+        else{
+            result+=char;
+        }
+    }
+    return result;
+}
+let nhapKyTu=prompt("Nhập kí tự cần mã hóa");
+let kq=maHoa(nhapKyTu);
+alert(`Kí tự trước khi mã hóa là: ${nhapKyTu}`);
+alert(`Kí tự sau khi mã hóa: ${kq}`);
